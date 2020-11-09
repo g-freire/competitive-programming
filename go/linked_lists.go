206cd60cb7a940eb48cf4550064b2ee17c5e6d06
g-freire <gustavomfreire@gmail.com>
2020-11-08 23:04:40 -0300

    starting LL golang implementation

---
+++ go/linked_lists.go
@@ -0,0 +1,122 @@
+package main
+
+import "fmt"
+
+// SINGLE LINKED LIST
+// https://golangbyexample.com/singly-linked-list-in-golang/
+
+type node struct {
+	name string
+	next *node
+}
+
+type singleList struct {
+	len  int
+	head *node
+}
+
+func initList() *singleList {
+	return &singleList{}
+}
+
+// METHODS
+func (s *singleList) Size() int {
+	return s.len
+}
+
+func (s *singleList) Traverse() error{
+	if s.head == nil {
+		fmt.Errorf("Cannot traverse empty LL")
+	}else{
+		current := s.head
+		for current != nil{
+			fmt.Print(current.name, " - ")
+			current = current.next
+		}
+	}
+	return nil
+}
+
+func (s *singleList) AddFront(name string) {
+	node := &node{
+		name: name,
+	}
+	if s.head == nil {
+		s.head = node
+	}else{
+		node.next = s.head
+		s.head = node
+	}
+	s.len++
+	return
+}
+
+func (s *singleList) AddBack(name string){
+	node := &node{
+		name: name,
+	}
+	if s.head == nil {
+		s.head = node
+	} else{
+		// traverse until next is nil
+		current := s.head
+		for current.next != nil{
+			current = current.next
+		}
+		current.next = node
+	}
+	s.len++
+	return
+}
+
+func main() {
+	singleList := initList()
+
+	//ADD HEAD METHOD
+	fmt.Printf("Size: %d\n", singleList.Size())
+	fmt.Printf("AddFront: A\n")
+	singleList.AddFront("A")
+	fmt.Printf("AddFront: B\n")
+	singleList.AddFront("B")
+	fmt.Printf("Size: %d\n", singleList.Size())
+
+
+	//ADD TAIL METHOD
+	fmt.Printf("AddBack: C\n")
+	singleList.AddBack("C")
+	err := singleList.Traverse()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	//
+	//fmt.Printf("RemoveFront\n")
+	//err = singleList.RemoveFront()
+	//if err != nil {
+	//	fmt.Printf("RemoveFront Error: %s\n", err.Error())
+	//}
+	//
+	//fmt.Printf("RemoveBack\n")
+	//err = singleList.RemoveBack()
+	//if err != nil {
+	//	fmt.Printf("RemoveBack Error: %s\n", err.Error())
+	//}
+	//
+	//fmt.Printf("RemoveBack\n")
+	//err = singleList.RemoveBack()
+	//if err != nil {
+	//	fmt.Printf("RemoveBack Error: %s\n", err.Error())
+	//}
+	//
+	//fmt.Printf("RemoveBack\n")
+	//err = singleList.RemoveBack()
+	//if err != nil {
+	//	fmt.Printf("RemoveBack Error: %s\n", err.Error())
+	//}
+	//
+	//err = singleList.Traverse()
+	//if err != nil {
+	//	fmt.Println(err.Error())
+	//}
+	//
+	//fmt.Printf("Size: %d\n", singleList.Size())
+}
--- python/sandbox.py
+++ python/sandbox.py
@@ -1,36 +1,41 @@
-
+###############################################
 # LINKED LIST
-
+###############################################
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
 
     def __repr__(self):
-        return self.data
+        return f"{self.data}"
 
 class LinkedList:
-    def __init__(self, nodes=None):
+    def insert_head(self):
         self.head = None
 
-        if nodes is not None:
-            node = Node(data=nodes.pop(0)) # removes from initial index to be head
-            self.head = node
+    def insert_tail(self):
+        pass
 
-            for data in nodes:
-                node.next = Node(data=data)
-                node = node.next
-    
-    def __repr__(self):
-        node = self.head
-        nodes = []
-        while node is not None:
-            nodes.append(node.data)
-            node = node.next
-        nodes.append("None")
-        return " -> ".join(nodes)
+
+
+###############################################
+# BINARY SEARCH
+###############################################
+def binary_search(list_input, target):
+    left = 0
+    right = len(list_input) + 1
+    while left <= right:
+        mid = (left + right) // 2
+        a  = list_input[mid]
+        if list_input[mid] == target:
+            return f"Found at {mid}"
+        elif list_input[mid] < target:
+            left = mid + 1
+        elif list_input[mid] > target:
+            right = mid - 1
 
 
 if __name__ == '__main__':
-    llist = LinkedList(["a", "b", "c", "d", "e"])
-    print(llist)
+    list_input = [1,3,5,6,7,8,12,15,22,33]
+    a = binary_search(list_input, 15)
+    print(a)
